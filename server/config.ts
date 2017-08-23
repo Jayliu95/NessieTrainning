224272ee1828ceca3eb69023f7e5704cbfd9339d
Jason Liu <xgl1@rutgers.edu>
2017-08-22 23:48:24 -0400

    Switched over to stockwatcher mlab collection

--- server/config.ts
+++ server/config.ts
@@ -2,9 +2,12 @@ export const serverPort = 4300;
 export const secret = "RbBQqA6uF#msRF8s7h*?@=95HUm&DgMDd6zLFn4XzWQ6dtwXSJwBX#?gL2JWf!";
 export const length = 128;
 export const digest = "sha256";
-
 export const mongoSecret = {
-  "mlabKey"   :   "mongodb://root:pass4you@d," +
-  "s121192.mlab.com:21192/blog",
+  "mlabKey"   :   "mongodb://root:pass4you@ds133450.mlab.com:33450/stockwatcher",
   "collection":   "users"
 };
+
+export const robinSecret = {
+  username: 'monmon_2007',
+  password: 'marina_zoom'
+}
